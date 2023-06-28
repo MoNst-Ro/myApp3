ceb8b34bc7bca33875ec5934c4bdebcc5eb63a4e
Nunzio Matera <nunziomatera5514@gmail.com>
2023-06-28 18:18:21 +0200

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,63 @@
 import { Component } from '@angular/core';
+import { User } from './model/user'
 
 @Component({
   selector: 'app-root',
   template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center" class="content">
-      <h1>
-        Welcome to {{title}}!
-      </h1>
-      <span style="display: block">{{ title }} app is running!</span>
-      <img width="300" alt="Angular Logo" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
-    </div>
-    <h2>Here are some links to help you start: </h2>
-    <ul>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial">Tour of Heroes</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/cli">CLI Documentation</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a></h2>
-      </li>
-    </ul>
-    <router-outlet></router-outlet>
+  <button class="btn btn-primary" [style.fontSize]="'50px'">
+  <i class="fa fa-home">
+  Button
+  </i>
+  </button>
+  <li 
+    *ngFor="let user of users" 
+    [class.male]="user.gender === 'M'"
+    [class.female]="user.gender === 'F'"
+  >
+  {{user.name}}
+  </li>
+<hr>
+  <li 
+    *ngFor="let user of users" 
+    [ngClass]="{
+      'male': user.gender === 'M',
+      'female': user.gender === 'F',
+      'big': true
+    }"
+  >
+  {{user.name}}
+  </li>
+  <hr>
+  <li 
+    *ngFor="let user of users" 
+    [style.color]="user.gender === 'M' ? 'blue' : 'purple'"
+    [style.background-color]="user.gender === 'F' ? 'pink' : 'red'"
+    [style.font-size]="user.gender === 'M' ? '40px' : '50px'"
+  >
+  {{user.name}}
+  </li>
+  <hr>
+  <li 
+    *ngFor="let user of users" 
+    [style.color]="user.gender === 'M' ? 'blue' : 'purple'"
+    [style.background-color]="user.gender === 'F' ? 'pink' : 'red'"
+    [style.font-size.px]="user.age"
+  >
+  {{user.name}}
+  </li>
   `,
-  styles: []
+  styles: [`
+    .male { background-color: blue }
+    .female { background-color: pink }
+    .big { font-size: 30px }
+  `]
 })
 export class AppComponent {
-  title = 'myApp3';
+  
+  users: User[] = [
+    {id: 1, name:'Fabio', age: 30, gender:'M'},
+    {id: 2, name:'Lucia', age: 20, gender:'F'},
+    {id: 3, name:'Luca', age: 50, gender:'M'},
+    {id: 4, name:'Anna', age: 100 ,gender:'F'}
+  ]
 }
